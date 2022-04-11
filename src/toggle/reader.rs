839add55023ef9274041b25f12e277010b82d3e2
theo5970 <theo5970@naver.com>
2022-04-11 21:00:45 +0900

    removed println! outputs.

--- src/toggle/reader.rs
+++ src/toggle/reader.rs
@@ -31,9 +31,6 @@ pub fn parse_toggle(x: &Vec<u8>) -> ToggleLevel {
     let mut width = 1;
     let mut height = 1;
     let mut total_buttons = 1;
-
-    println!("Version: {}", x[i]);
-
     i += 1;
 
     let mut should_escape = false;
@@ -43,31 +40,23 @@ pub fn parse_toggle(x: &Vec<u8>) -> ToggleLevel {
             WIDTH_DEF => {
                 i += 1;
                 width = x[i];
-                println!("Width: {}", x[i]);
             }
             HEIGHT_DEF => {
                 i += 1;
                 height = x[i];
-                println!("Height: {}", x[i]);
             }
             SUBTYPES_DEF => {
-                print!("SubTypes: ");
                 total_buttons = width * height;
 
                 let start = i + 1;
                 let end = start + (total_buttons as usize);
                 let subtypes_range = &x[start..end];
 
-                for _ in 0..total_buttons {
-                    i += 1;
-                    print!("{}, ", x[i]);
-                }
+                i += total_buttons as usize;
 
                 result.subtypes.extend_from_slice(subtypes_range);
-                println!("");
             }
             STATES_DEF => {
-                print!("States: ");
                 let num_bytes: u32 = u32::from((total_buttons - 1) / 8 + 1);
 
                 let start = i + 1;
@@ -75,13 +64,8 @@ pub fn parse_toggle(x: &Vec<u8>) -> ToggleLevel {
                 let states_range = &x[start..end];
 
                 result.states = BitArray::from(states_range);
-                result.states.print();
 
-                for _ in 0..num_bytes {
-                    i += 1;
-                    print!("{}, ", x[i]);
-                }
-                println!("");
+                i += num_bytes as usize;
             }
             MINIMUM_CLICK_DEF => {
                 let start = i + 1;
@@ -91,14 +75,10 @@ pub fn parse_toggle(x: &Vec<u8>) -> ToggleLevel {
 
                 let a = i32::from_le_bytes(integer_bytes);
                 result.min_clicks = u32::try_from(a).unwrap();
-                
-                println!("Minimum Clicks: {}", a);
-
                 i += 4;
             }
 
             CREATOR_DEF => {
-                println!("Creator");
                 should_escape = true;
             }
 
